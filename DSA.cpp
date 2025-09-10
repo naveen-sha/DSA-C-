c2f0f7edf97d8ffec159114a13e7ff2914c657a4
naveen6010 <naveen972821@gmail.com>
2025-09-10 07:59:38 +0530

    datatype in c++

--- DSA.cpp
+++ DSA.cpp
@@ -1,5 +1,27 @@
-#include <iostream>;
+ #include<iostream>
+using namespace std;
+/* int main(){
+    cout << "namaste duniya" <<endl;
+    
+};*/
+
 int main(){
-    printf("naveen");
-    return 0; 
+    int a =123;
+    cout << a<<endl;
+    char b='v';
+    cout << b<<endl;
+    bool bl=true;
+    cout<< bl <<endl;
+    float fl=2.0;
+    cout << fl <<endl;
+    double d=1.23;
+    cout << d << endl;
+    int size = sizeof(d);
+    cout<< size <<endl;
+
+ 
+
+   
+
+
 };
