9069471317990d2a24a1def60749d572abd358d1
naveen6010 <naveen972821@gmail.com>
2025-09-24 14:05:49 +0530

    push

--- DSA.cpp
+++ DSA.cpp
@@ -147,7 +147,43 @@ if(a>0){
     i++;
    }*/
 //    create a square star pattern.
+/* int main(){
+    int n ;
+    cin>>n;
+    int i =1;
+    while (i<=n)
+    {
+        int j=1;
+        while (j<=n)
+        {
+            cout<<"*";
+            j++;
+        }
+        cout<<endl;
+        i++;
+    }
+
+    
+
+}*/
 int main(){
+    int n;
+    cin>>n;
+    int i=1;
+    while (i<=n)
+    {
+        int j=1;
+        while (j<=n)
+        {
+            cout<<i;
+            j++;
+
+        }
+        cout<<endl;
+        i++;
+    
+    }
+    
 
 }
 
