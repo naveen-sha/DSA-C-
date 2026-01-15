eebb092f70bbedbfa84564863d88dbba8a2e8cbe
naveen-sha <naveen972821@gmail.com>
2026-01-15 20:34:26 +0530

    check

--- DSA.cpp
+++ DSA.cpp
@@ -3,33 +3,9 @@ using namespace std;
 
 
 int main (){
-    int n;
-    cin>>n;
-    int i=1;
-    while (i<=n)
-    {
-        int j=1;
-        while (j<=n-i+1)
-        {
-            cout<<j;
-            j++;
-        }
-        int star=2;
-        while (star<=i)
-        {
-            cout<<"*"<<"*";
-            star++;
-        }
-        int k=n-i+1;
-        while (k)
-        {
-            cout<<k;
-            k--;
-        }
-        
-        cout<<endl;
-        i++;
-    }
     
+    cout << "Hello, DSA!" << endl;
+    return 0;
+
 
    }
