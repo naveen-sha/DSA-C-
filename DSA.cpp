2c0f473a84b95794e1d012ae8a1bb36eaffe4fbc
naveen-sha <naveen972821@gmail.com>
2026-01-17 21:18:31 +0530

    push

--- DSA.cpp
+++ DSA.cpp
@@ -1,14 +1,8 @@
- #include<iostream>
+#include<iostream>
 using namespace std;
-
 
 int main (){
     
-    cout << "Hello, DSA!" << endl;
+    cout << "Hello World" << endl;
     return 0;
-
-
-
-
    };
-   
