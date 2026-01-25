f2327daddfbe60625cb282a1ec180a52fef0766b
naveen-sha <naveen972821@gmail.com>
2026-01-25 16:07:24 +0530

    add dsa files

--- variable-datatype-conditional/c.cpp
+++ variable-datatype-conditional/c.cpp
@@ -1,7 +1,7 @@
 #include<iostream>
 using namespace std;
 
-/* int main (){
+ int main (){
     
    cout << "*****" << endl;
    cout << "****" << endl;
@@ -9,28 +9,9 @@ using namespace std;
    cout << "**" << endl;
    cout << "*" << endl;
     return 0;
-   };*/
-   /* int main(){
-   cout << "*****" << endl;
-   cout << "   *" << endl;
-   cout << "  *" << endl;
-   cout << " *" << endl;
-   cout << "*****" << endl;
-    return 0;
-   }
-    */
-   /*int main(){
-   cout << "5 * 1 = 5" << endl;
-   cout << "5 * 2 = 10" << endl;
-   cout << "5 * 3 = 15" << endl;
-   cout << "5 * 4 = 20" << endl;
-   cout << "5 * 5 = 25" << endl;
-   cout << "5 * 6 = 30" << endl;
-   cout << "5 * 7 = 35" << endl;
-   cout << "5 * 8 = 40" << endl;
-   cout << "5 * 9 = 45" << endl;
-   cout << "5 * 10 = 50" << endl;
-   };*/
+   };
+
+   
 
    /* int main(){
       int length;
---
+++ variable-datatype-conditional/f.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+      int length;
+      int breadth;
+      cin >> breadth;
+      cin >> length;
+      int area = length * breadth;
+      int perimeter = 2 * (length + breadth);
+      cout<< "Area = " << area << endl;
+      cout<< "Perimeter = " << perimeter << endl;
+   };
+   
